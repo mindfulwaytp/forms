0fb7ea3fbb0e3ec156049809f86dcae6319ad209
MindfulwayTP <ryne@mindfulway-therapy.com>
2025-07-03 14:03:30 -0700

    remove admin

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/admin/', // 👈 This is the key fix
+  base: '/', // or just omit this line entirely (Vite defaults to '/')
 });
